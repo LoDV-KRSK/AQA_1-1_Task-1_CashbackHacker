1d1620e65a0be31e1246d22cb20b103930885c68
lodv-krsk <dloshenov@ya.ru>
2022-03-18 20:23:19 +0700

    added junit4-platform

--- src/test/java/ru/netology/unit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -1,56 +1,76 @@
 package ru.netology.unit;
 
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
 
+    CashbackHackService service = new CashbackHackService();
+
+
+    /* JUnit 4 */
+
     @Test
     public void shouldNotReturnAnything() {
-        CashbackHackService service = new CashbackHackService();
-        int actual = service.remain(1000);
-        int expected = 0;
-        assertEquals(expected, actual);
+        Assertions.assertEquals(0, service.remain(1000));
     }
 
     @Test
     public void shouldReturnOne() {
-        CashbackHackService service = new CashbackHackService();
-        int actual = service.remain(999);
-        int expected = 1;
-        assertEquals(expected, actual);
+        Assertions.assertEquals(1, service.remain(999));
     }
 
     @Test
     public void shouldReturnFiveHundred() {
-        CashbackHackService service = new CashbackHackService();
-        int actual = service.remain(500);
-        int expected = 500;
-        assertEquals(expected, actual);
+        Assertions.assertEquals(500, service.remain(500));
     }
 
     @Test
     public void shouldReturn999forSmallPurchase() {
-        CashbackHackService service = new CashbackHackService();
-        int actual = service.remain(1);
-        int expected = 999;
-        assertEquals(expected, actual);
+        Assertions.assertEquals(999, service.remain(1));
     }
 
     @Test
     public void shouldReturn999forLargePurchase() {
-        CashbackHackService service = new CashbackHackService();
-        int actual = service.remain(1001);
-        int expected = 999;
-        assertEquals(expected, actual);
+        Assertions.assertEquals(999, service.remain(1));
     }
 
     @Test
     public void shouldReturn450() {
-        CashbackHackService service = new CashbackHackService();
-        int actual = service.remain(1550);
-        int expected = 450;
-        assertEquals(expected, actual);
+        Assertions.assertEquals(450, service.remain(1550));
+    }
+
+    /* JUnit 5 */
+
+    @org.junit.jupiter.api.Test
+    public void shouldNotReturnAnything_JU5() {
+        Assertions.assertEquals(0, service.remain(1000));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldReturnOne_JU5() {
+        Assertions.assertEquals(1, service.remain(999));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldReturnFiveHundred_JU5() {
+        Assertions.assertEquals(500, service.remain(500));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldReturn999forSmallPurchase_JU5() {
+        Assertions.assertEquals(999, service.remain(1));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldReturn999forLargePurchase_JU5() {
+        Assertions.assertEquals(999, service.remain(1001));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldReturn450_JU5() {
+        Assertions.assertEquals(450, service.remain(1550));
     }
 }
